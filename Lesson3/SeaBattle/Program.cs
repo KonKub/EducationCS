0d0fc6e7628fbbe80bc34f07ef1f4e8156f3a832
Konstantin <iki0@yandex.ru>
2021-10-01 11:52:19 +0400

    Lesson3

--- Lesson3/SeaBattle/Program.cs
+++ Lesson3/SeaBattle/Program.cs
@@ -10,10 +10,15 @@ namespace SeaBattle
     {
         static void Main(string[] args)
         {
-            const int X = 10;
-            const int Y = 10;
+            const int X = 10;                                     //размер игрового поля по X
+            const int Y = 10;                                     //размер игрового поля по Y
+
             const int ShipsNum = 10;                              //количество кораблей
             int[] ship = new int[ShipsNum] {4,3,3,2,2,2,1,1,1,1}; //палубы кораблей
+
+            char[] CA = new char[3] {'O',                         //свободные клетки
+                                     'X',                         //корабль
+                                     'O'};                        //место вокруг корабля
             int[,] A = new int[X,Y];                              
             Random rand = new Random();
 
@@ -24,24 +29,34 @@ namespace SeaBattle
             bool TryAgain;
             int orient;
             int posX;
-            int posY;          //ShipsNum
+            int posY;
+            int iteration = 0;
             for (int n = 0; n < ShipsNum; n++) {
                 for (; ; ) {
+                    if (iteration > 200)
+                    {
+                        Console.WriteLine("Не удалось расставить корабли на игровом поле.");
+                        Console.ReadKey();
+                        Environment.Exit(1);
+                    }
+                    iteration++;
                     TryAgain = false;
                     orient = rand.Next(2); //0 - горизонтальное, 1 - вертикальное расположение корабля
                     posX = rand.Next(X);
                     posY = rand.Next(Y);
+
+                    //Корректируем координаты, чтобы не выходили за пределы игрового поля и проверяем занято место или нет
                     if (orient == 0)
                     {
                         if (posX > X - ship[n] - 1) posX = X - ship[n];
-                        for (int i = posX; i < posX + ship[n]; i++)  //Проверяем занято место или нет
+                        for (int i = posX; i < posX + ship[n]; i++)  
                             if (A[i, posY] != 0) { TryAgain = true; break; }
                         if (TryAgain) continue;
                     }
                     else
                     {
                         if (posY > Y - ship[n] - 1) posY = Y - ship[n];
-                        for (int i = posY; i < posY + ship[n]; i++)  //Проверяем занято место или нет
+                        for (int i = posY; i < posY + ship[n]; i++)
                             if (A[posX, i] != 0) { TryAgain = true; break; }
                         if (TryAgain) continue;
                     }
@@ -72,9 +87,6 @@ namespace SeaBattle
                 }
             }
 
-            char[] CA = new char[3] {'O', //свободные клетки
-                                     'X', //корабль
-                                     'O'};//место вокруг корабля
             for (int i = 0; i < Y; i++)
             {
                 for (int j = 0; j < X; j++) Console.Write(" "+ CA[A[j,i]]);
